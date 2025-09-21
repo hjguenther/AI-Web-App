a8dea3956b66322b853133f71dae8c61e01a13d4
Heidi <heidijoyguenther@gmail.com>
2025-09-21 16:02:30 -0600

    Update app.py
    updated to fancy html code

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-# app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "This will be replaced with some fancy AI magic 🎉"}
+# Tell FastAPI where templates are
+templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    # Must pass {"request": request}, or it hangs
+    return templates.TemplateResponse(
+        "index2.html",
+        {"request": request, "message": "This will be replaced with some fancy AI magic"}
+    )
