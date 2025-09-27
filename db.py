0972a9f8ed06990027e173f6db152748ddc96d50
Heidi <heidijoyguenther@gmail.com>
2025-09-27 16:27:46 -0600

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-import os
-
-# Render will inject this securely
-db_url = os.getenv("DATABASE_URL")
-
-# Connect with SSL required (Render uses SSL by default)
-engine = create_engine(db_url, connect_args={"sslmode": "require"})
